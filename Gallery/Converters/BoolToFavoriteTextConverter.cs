b5b36761d15800c7caead1cf7bcddef953f550af
ShkrebnevAleksander <shkrebnev.aleksander@gmail.com>
2025-10-09 22:58:04 +0300

    feat: Added Converters for FavoriteButton

---
+++ Gallery/Converters/BoolToFavoriteTextConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Gallery.Converters;
+
+public class BoolToFavoriteTextConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool isFavorite)
+        {
+            return isFavorite ? "icon_heartfilled.png" : "icon_unheartfilled.png";
+        }
+        return "icon_unheartfilled.png"; 
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
